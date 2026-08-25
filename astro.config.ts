84698a0e82217a9f83a4c82ea79871019541325d
jmferreirab <29668777+jmferreirab@users.noreply.github.com>
2026-08-24 21:38:19 -0500

    feat(i18n): restructure index page and update astro.config for i18n support

--- astro.config.ts
+++ astro.config.ts
@@ -1,13 +1,15 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
-
 export default defineConfig({
   site: 'https://jmferreirab.github.io',
   base: '/portfolio',
   integrations: [],
-  vite: {
-    plugins: [tailwindcss()],
-  },
+  vite: { plugins: [tailwindcss()] },
   compressHTML: true,
-  prefetchAll: true,
+  prefetch: true,
+  i18n: {
+    locales: ['en', 'es'],
+    defaultLocale: 'en',
+    routing: { prefixDefaultLocale: true, redirectToDefaultLocale: true },
+  },
 });
