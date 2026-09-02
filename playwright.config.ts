1d82800d08a4a0c875223ba29bd72dbb281b91a3
jmferreirab <29668777+jmferreirab@users.noreply.github.com>
2026-09-02 15:33:00 -0500

    feat: update Playwright configuration

--- playwright.config.ts
+++ playwright.config.ts
@@ -6,10 +6,11 @@ export default defineConfig({
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
-  reporter: 'html',
+  reporter: [['html', { open: 'never' }]],
   use: {
-    baseURL: 'http://localhost:4321',
+    baseURL: 'http://localhost:4321/portfolio/',
     trace: 'on-first-retry',
+    screenshot: 'only-on-failure',
   },
   projects: [
     {
